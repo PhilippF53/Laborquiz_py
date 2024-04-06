fa50b3c55b92931e92c5f8637750b9ab78fc418d
PhilippF53 <3002084@stud.hs-mannheim.de>
2024-04-06 17:06:25 +0200

    Ergänzung UI Methoden

--- .venv/quiz_ui.py
+++ .venv/quiz_ui.py
@@ -25,8 +25,8 @@ class QuizInterface:
         self.userAnswer = StringVar()
 
         #Display four options(radio buttons)
-        self.opts = self.radio_buttons()
-        self.display_options()
+        self.opts = self.radioButtons()
+        self.displayAnswers()
 
         #To show whether the answer is correct or wrong
         self.feedback = Label(self.window, pady=10, font=("ariel", 15, "bold"))
@@ -42,6 +42,60 @@ class QuizInterface:
         qText = self.quiz.nextQuestion
         self.canvas.itemconfig(self.questionText, text=qText)
         
-    def 
+    def radioButtons(self):
+        #initialize an empty list of answers
+        answers = []
+        yPos = 220
+        
+        #initialize 4 Answer fields
+        while (len(answers) < 4):
+            radioButton = Radiobutton(self.window, text="", variable=self.userAnswer,
+                                    value='', font=("ariel", 14))
+            answers.append(radioButton)
+            radioButton.place(x=200, y=yPos)
+            yPos += 40
+        
+        return answers
+    
+    def displayAnswers(self):    
+        #displaying the four answers
+        val = 0
+        self.userAnswer.set(None)
+        #looping through answer options and filling text of radio buttons
+        for option in (self.quiz.currentQuestion.solution & self.quiz.currentQuestion.incorrect_answers):
+            self.opts[val]['text'] = option
+            self.opts[val]['value'] = option
+            val += 1
+            
+    def nextButton(self):
+        #check if answer is correct
+        if self.quiz.checkAnswer(self.userAnswer.get()):
+            self.feedback["fg"] = "green"
+            self.feedback["text"] = 'Correct answer! \U0001F44D'
+        else:
+            self.feedback["fg"] = "red"
+            self.feedback["text"] = ('\u274E Ooops!\n'
+                                     f'The right answer is: {self.quiz.currentQuestion.solution}')
+        #check if there are more questions left
+        if self.quiz.hasNextQuestion():
+            self.displayQuestion()
+            self.displayAnswers()
+        #if not display the result
+        else:
+            self.displayResult()
+            
+    def buttons(self):
+        #show the different buttons
+        #first the Next button to move to the next question
+            
+        
+    def displayResult(self):
+        #display the result using a messagebox
+        correct, incorrect = self.quiz.getResult()
+        correct = f"Correct: {correct}"
+        incorrect = f"Incorrect: {incorrect}"
+        
+        messagebox.showinfo("Result", f"{correct}\n{incorrect}")
+            
         
         
